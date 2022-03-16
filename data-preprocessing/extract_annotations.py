6388f619495b2d0d0aafd56f61dd3ff21f74900e
Hannes Kuchelmeister <hannes@kuchelmeister.org>
2022-03-16 15:33:15 +0000

    rewrite code to use folder and support using multiple input files

--- data-preprocessing/extract_annotations.py
+++ data-preprocessing/extract_annotations.py
@@ -3,6 +3,7 @@ import os
 from itertools import chain
 from dotenv import load_dotenv
 import pandas as pd
+import numpy as np
 
 
 def to_relative_focus(stack):
@@ -30,15 +31,51 @@ def flatten_stack(stack):
 
 if __name__ == "__main__":
     load_dotenv()
-    data_file = os.getenv("DATA_FILE")
-    out_file = os.getenv("OUT_FILE")
 
-    with open(data_file) as f:
-        content = json.load(f)
+    train_split = float(os.getenv("TRAIN_SPLIT"))
+    val_split = float(os.getenv("VALIDATION_SPLIT"))
+    test_split = float(os.getenv("TEST_SPLIT"))
 
-    annotated = filter(lambda x: x["best_index"], content)
-    relative_focus = map(to_relative_focus, annotated)
-    flattened = chain(*map(flatten_stack, relative_focus))
+    assert abs(1 - (train_split + val_split + test_split)) < 0.01
 
-    dataframe = pd.DataFrame(flattened)
-    dataframe.to_csv(out_file)
+    in_folder = os.getenv("IN_FOLDER")
+    out_folder = os.getenv("OUT_FOLDER")
+
+    train_list = []
+    val_list = []
+    test_list = []
+
+    for file in os.listdir(in_folder):
+        if file.endswith(".json"):
+            with open(os.path.join(in_folder, file)) as f:
+                content = json.load(f)
+
+            annotated = filter(lambda x: x["best_index"], content)
+            relative_focus = map(to_relative_focus, annotated)
+            flattened = list(chain(*map(flatten_stack, relative_focus)))
+
+            # https://stackoverflow.com/a/49556954
+            train, validate, test = np.split(
+                flattened,
+                [
+                    int(len(flattened) * train_split),
+                    int(len(flattened) * (train_split + val_split)),
+                ],
+            )
+
+            train_list.extend(train)
+            val_list.extend(validate)
+            test_list.extend(test)
+
+    np.random.shuffle(train_list)
+    np.random.shuffle(val_list)
+    np.random.shuffle(test_list)
+
+    dataframe = pd.DataFrame(train_list)
+    dataframe.to_csv(os.path.join(out_folder, "train_metadata.csv"))
+
+    dataframe = pd.DataFrame(val_list)
+    dataframe.to_csv(os.path.join(out_folder, "validation_metadata.csv"))
+
+    dataframe = pd.DataFrame(test_list)
+    dataframe.to_csv(os.path.join(out_folder, "test_metadata.csv"))
